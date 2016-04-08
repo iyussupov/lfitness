881e9ccc959e5ca111ccd03192669b92e0878ead
Ingwar <Dev1@Dev1s-iMac.local>
2016-04-08 10:09:23 +0300

    init

---
+++ Pods/Target Support Files/Pods-lfitness/Pods-lfitness-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_lfitnessVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_lfitnessVersionString[];
+
---
+++ lfitness/ImageRadius.swift
@@ -0,0 +1,17 @@
+//
+//  ImageRadius.swift
+//  lfitness
+//
+//  Created by Dev1 on 3/18/16.
+//  Copyright © 2016 fxofficeapp. All rights reserved.
+//
+
+import UIKit
+
+class ImageRadius: UIImageView {
+    
+    override func awakeFromNib() {
+        layer.cornerRadius = 60.0
+    }
+
+}
